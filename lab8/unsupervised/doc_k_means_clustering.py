b00adcb61242a42cc419805dc64c401a4b983054
Alisa Krivokapic <alisa.kriv@gmail.com>
2018-03-28 16:28:53 -0400

    changes to lab 8

--- lab8/unsupervised/doc_k_means_clustering.py
+++ lab8/unsupervised/doc_k_means_clustering.py
@@ -1,18 +1,10 @@
-import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA, TruncatedSVD
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, TfidfTransformer
 from sklearn.pipeline import Pipeline
+import numpy as np
 
-
-### For the purposes of this example, we store feature data from our
-### dataframe `df`, in the `f1` and `f2` arrays. We combine this into
-### a feature matrix `X` before entering it into the algorithm.
-# get the data, divide it into training and testing data sets
-#df = pd.read_csv('multi_class_data.csv')
-##set up colors per clusters using a dict
-#cluster_colors = {0: '#1b9e77', 1: '#d95f02', 2: '#7570b3'}
-#cluster_names = df.label.map({'breast':0, 'foot':1, 'ear':2})
+from random import shuffle
 
 #documents = ["Human machine interface for lab abc computer applications",
 #             "A survey of user opinion of computer system response time",
@@ -25,12 +17,13 @@ from sklearn.pipeline import Pipeline
 #             "Graph minors A survey"]
 
 documents = []
-with open("ear.txt", "r") as f:
+with open("data/ear.txt", "r") as f:
     documents.extend(f.read().splitlines())
-with open("brain.txt", "r") as f:
+with open("data/brain.txt", "r") as f:
     documents.extend(f.read().splitlines())
-with open("eye.txt", "r") as f:
+with open("data/eye.txt", "r") as f:
     documents.extend(f.read().splitlines())
+shuffle(documents)
 
 vectorizer = TfidfVectorizer(stop_words='english')
 X = vectorizer.fit_transform(documents)
@@ -44,16 +37,7 @@ order_centroids = model.cluster_centers_.argsort()[:, ::-1]
 terms = vectorizer.get_feature_names()
 for i in range(true_k):
     print("Cluster %d:" % i)
-    for ind in order_centroids[i, :3]:
+    for ind in order_centroids[i, :10]:
         print(' %s' % terms[ind])
     print()
 
-#centers2D = pca.transform(kmeans.cluster_centers_)
-#colors = df.apply(lambda row: cluster_colors[row.label], axis=1)
-#
-#ax = df.plot(kind='scatter', alpha=0.1, s=300, c=colors)
-#plt.show()
-#plt.hold(True)
-#plt.scatter(centers2D[:,0], centers2D[:,1], 
-#            marker='x', s=200, linewidths=3, c='r')
-#plt.show() 
--- lab8/unsupervised/plotting.py
+++ lab8/unsupervised/plotting.py
@@ -1,32 +1,62 @@
-# -*- coding: utf-8 -*-
-
 from sklearn.datasets import fetch_20newsgroups
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, TfidfTransformer
 from sklearn.decomposition import PCA
 from sklearn.pipeline import Pipeline
-import matplotlib.pyplot as plt
-
-newsgroups_train = fetch_20newsgroups(subset='train', 
-                                      categories=['alt.atheism', 'sci.space'])
-
-pipeline = Pipeline([
-    ('vect', CountVectorizer()),
-    ('tfidf', TfidfTransformer()),
-])        
-X = pipeline.fit_transform(newsgroups_train.data).todense()
-
 from sklearn.cluster import KMeans
 
-kmeans = KMeans(n_clusters=2)
-kmeans.fit(X)
+import nltk
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
 
+import matplotlib.pyplot as plt
+
+from random import shuffle
+import string
+import re
+
+# create our own set of documents 
+documents = []
+with open("data/eye.txt", "r") as f:
+    documents.extend(f.read().splitlines())
+with open("data/brain.txt", "r") as f:
+    documents.extend(f.read().splitlines())
+shuffle(documents)
+
+# transform our text into vectors & transform with tfidf 
+pipeline = Pipeline([
+    ('vect', TfidfVectorizer()),
+    ('tfidf', TfidfTransformer()),
+])        
+X = pipeline.fit_transform(documents).todense()
+
+# set a k
+true_k = 2
+
+# fit our kmeans model to our transformed data
+kmeans_model = KMeans(n_clusters=true_k)
+kmeans_model.fit(X)
+
+# reduce our data to 2 dimensions so we can visualize it  
 pca = PCA(n_components=2).fit(X)
 data2D = pca.transform(X)
-plt.scatter(data2D[:,0], data2D[:,1], c=kmeans.labels_)
+centers2D = pca.transform(kmeans_model.cluster_centers_)
 
-centers2D = pca.transform(kmeans.cluster_centers_)
-
-plt.hold(True)
+plt.scatter(data2D[:,0], data2D[:,1], c=kmeans_model.labels_)
 plt.scatter(centers2D[:,0], centers2D[:,1], 
             marker='x', s=200, linewidths=3, c='r')
 plt.show()   
+
+# print top terms
+vectorizer = TfidfVectorizer(stop_words='english')
+X_2 = vectorizer.fit_transform(documents)
+kmeans_model.fit(X_2)
+
+print("Top terms per cluster:")
+order_centroids = kmeans_model.cluster_centers_.argsort()[:, ::-1]
+terms = vectorizer.get_feature_names()
+#terms = pipeline.named_steps['vect'].get_feature_names()
+for i in range(true_k):
+    print("Cluster %d:" % i)
+    for ind in order_centroids[i, :5]:
+        print(' %s' % terms[ind])
+    print()
